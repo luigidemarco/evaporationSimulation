3c2d103329ff9587536a9b4218d9146834b582d2
Luigi <luigidemarco2@gmail.com>
2020-04-20 06:55:04 -0600

    Updated values.

--- process_output.py
+++ process_output.py
@@ -13,7 +13,7 @@ omega = 2 * np.pi * 35
 m = 127 * 1.6E-27
 
 filePath = './results/'
-files = ['NoEvaporation_20200416_01.out']
+files = ['NoEvaporation_20200417_00.out']
 excludeTime = 0.04
 
 for k in files:
